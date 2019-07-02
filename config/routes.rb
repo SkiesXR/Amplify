e895c54c5770522507ec5fc4818f823daaf8ee56
SkiesXR <pkrasnick@gmail.com>
2019-07-02 15:39:22 -0700

    Add ability to view Root html

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'root#root'
-  
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :destroy, :edit, :update, :show]
+  namespace :api, defaults: {format: :json} do
+    resource :user, only: [:create]
+    resource :session, only: [:create, :destroy]
   end
 
-  resource :session, only: [:create, :destroy]
+  root "static_pages#root"
 end
