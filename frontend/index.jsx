7ecf8c89823b7e73cec8e36c2fe2d98013873fb8
SkiesXR <pkrasnick@gmail.com>
2019-07-02 20:00:02 -0700

    Able to test store.getState successfully

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import * as SessionApiUtil from './util/session_api_util';
+import configureStore from './store/store';
 import { login, signup, logout } from './util/session_api_util';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // window.login = login;
-    // window.signup = signup;
-    // window.logout = logout;
+    const store = configureStore();
     const root = document.getElementById('root');
     ReactDOM.render(<h1>Music for everyone.</h1>, root);
+    window.getState = store.getState();    
 });
