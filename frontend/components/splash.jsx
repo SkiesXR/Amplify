d95f82b5739a9ef88d061ef659139cf671c7699f
SkiesXR <pkrasnick@gmail.com>
2019-07-04 20:42:36 -0700

    Finish nav and main splash content

--- frontend/components/splash.jsx
+++ frontend/components/splash.jsx
@@ -22,7 +22,11 @@ class Splash extends React.Component {
                     <div className="hero-home-bg-cover">
                     </div>
                 </div>
-                
+                <div className="splash-main">
+                    <h1 className="hero-h1">Music for everyone.</h1>
+                    <h4>Millions of songs. No credit card needed.</h4>
+                    <button className="splash-demo-btn">Demo Login</button>
+                </div>
                 
             </div>
         );
