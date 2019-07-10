d97c13564e857b5931c53188b18837da4aa19071
SkiesXR <pkrasnick@gmail.com>
2019-07-09 22:40:21 -0700

    Fix now playing button section location

--- frontend/components/Player/grid_item.jsx
+++ frontend/components/Player/grid_item.jsx
@@ -3,18 +3,18 @@ import { Link } from 'react-router-dom';
 
 class GridItem extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    // }
-
     render() {
+
         const { album_art, title, artist  } = this.props.album;
+
         return (
             <div className="album-artist-container">
                 <img src={ album_art }/>
+
                 <div class="title-container">
                     <Link id="grid-title" to="">{title}</Link>
                 </div>
+
                 <div class="artist-container">
                     <Link id="grid-artist" to="">{ artist }</Link>
                 </div>
