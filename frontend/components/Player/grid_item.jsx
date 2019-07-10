9ce95ff0968894d1d7e124f179314e9a51293c4a
SkiesXR <pkrasnick@gmail.com>
2019-07-09 21:05:51 -0700

    Fixed left-navg min-width

--- frontend/components/Player/grid_item.jsx
+++ frontend/components/Player/grid_item.jsx
@@ -11,9 +11,13 @@ class GridItem extends React.Component {
         const { album_art, title, artist  } = this.props.album;
         return (
             <div className="album-artist-container">
-                <img src={ album_art }></img>
-                <Link id="grid-title" to="">{ title }</Link>
-                <Link id="grid-artist" to="">{ artist }</Link>
+                <img src={ album_art }/>
+                <div class="title-container">
+                    <Link id="grid-title" to="">{title}</Link>
+                </div>
+                <div class="artist-container">
+                    <Link id="grid-artist" to="">{ artist }</Link>
+                </div>
             </div>
         );
     }
