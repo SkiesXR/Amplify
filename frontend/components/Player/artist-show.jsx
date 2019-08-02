ad3a03ff66ac396d5eb4fd50a20d82efece824fd
SkiesXR <pkrasnick@gmail.com>
2019-08-01 22:40:09 -0700

    Display artist's releases by release date

--- frontend/components/Player/artist-show.jsx
+++ frontend/components/Player/artist-show.jsx
@@ -8,15 +8,17 @@ class ArtistShow extends React.Component {
     }
 
     render() {
-        debugger;
         if (!this.props.artist) return "";
         let artist = this.props.artist || {};
         const { artist_photo, bio, name } = this.props.artist || "";
         const albums = artist.albums || {};
-        const orderedAlbums = Object.values(albums)
-        let albumList = Object.values(albums).map(album => {
-            return <li>{ album.title }</li>
+        let sortedAlbums = Object.values(albums).sort(function (x,y) {
+            return x.release_date.slice(0,4) - y.release_date.slice(0,4);
         })
+        let albumList = Object.values(sortedAlbums).map(album => {
+            return <li>{album.title} - {album.album_type} - {album.release_date}</li>
+        })
+
         return (
             <div className="artist-show-c1">
                 <div>{name}</div>
