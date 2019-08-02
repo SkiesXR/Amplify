90a1d1073335828c08c9f1a71ef7519ba7f384f4
SkiesXR <pkrasnick@gmail.com>
2019-08-01 21:22:09 -0700

    All artist details pulled from state and showing on page

--- frontend/components/Player/artist-show.jsx
+++ frontend/components/Player/artist-show.jsx
@@ -5,14 +5,23 @@ class ArtistShow extends React.Component {
     componentDidMount() {
         let artistId = this.props.match.params.artistId;
         this.props.fetchArtist(artistId);
-        debugger;
     }
 
     render() {
         debugger;
+        if (!this.props.artist) return "";
+        let artist = this.props.artist || {};
+        const { artist_photo, bio, name } = this.props.artist || "";
+        const albums = artist.albums || {};
+        let albumList = Object.values(albums).map(album => {
+            return <li>{ album.title }</li>
+        })
         return (
             <div className="artist-show-c1">
-                <span>Hey there</span>
+                <div>{name}</div>
+                <div>{bio}</div>
+                <div>{ albumList }</div>
+                <img src={ artist_photo }></img>
             </div>
         );
     }
