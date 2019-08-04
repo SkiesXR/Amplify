8ac0e293cfeac65633779f83ec1786380be9b538
SkiesXR <pkrasnick@gmail.com>
2019-08-04 14:16:59 -0700

    Add track artist, title, artwork, and duration to local state for player component

--- frontend/components/Player/player.jsx
+++ frontend/components/Player/player.jsx
@@ -15,20 +15,31 @@ class Player extends React.Component {
         this.mouseDown = this.mouseDown.bind(this);
         this.mouseUp = this.mouseUp.bind(this);
         this.mouseMove = this.mouseMove.bind(this);
+        this.timeUpdate = this.timeUpdate.bind(this);
+        // this.findDuration = this.findDuration.bind(this);
 
         // Refs
         this.rangeslider = React.createRef();
         this.rangesliderFill = React.createRef();
         this.rangesliderHandle = React.createRef();
         this.audio = React.createRef();
+        // this.duration = React.createRef();
         // this.barMovement = this.barMovement.bind(this);
         // const audio = <audio id="audio"><source src="skylines.mp3" /></audio>
+        // const duration = audio.duration;
 
         this.state = {
             play: false,
             audioSrc: "skylines.mp3",
-            // track: audio,
-            playheadPos: 0,
+            // length: duration,
+            track: {
+                title: "Skylines",
+                src: "skylines.mp3",
+                duration: "03:09",
+                artist: "Animalfirepower",
+                artwork: "https://amplifyskiesxr-seeds.s3-us-west-1.amazonaws.com/Album+Photos/AFP+-+Skylines.jpg"
+            },
+            playheadPos: "0px",
             playPauseButton: "play_white.png",
             currentSong: null,
             loveButton: "love.png",
@@ -43,13 +54,28 @@ class Player extends React.Component {
     }
 
     componentDidMount() {
-        this.audio.current.addEventListener("timeupdate", () => {
-            let ratio = this.audio.current.currentTime / this.audio.current.duration;
-            let position = this.rangeslider.current.offsetWidth * ratio;
+        // debugger;
+        let track;
+        track = document.querySelector('#audio');
+        track.addEventListener("timeupdate", () => {
+            console.log("time updated");
+            let rangeslider;
+            rangeslider = document.querySelector('.rangeslider');
+            let ratio = track.currentTime / track.duration;
+            let position = rangeslider.offsetWidth * ratio;
             this.positionHandle(position);
         });
     }
 
+
+    
+    timeUpdate() {
+        // debugger;
+        // let ratio = this.audio.current.currentTime / this.audio.current.duration;
+        // let position = this.rangeslider.current.offsetWidth * ratio;
+        // this.positionHandle(position);
+    }
+    
     // Display appropriate "like" button based user action
     toggleLove() {
         switch(this.state.loveButton) {
@@ -175,29 +201,47 @@ class Player extends React.Component {
     // let duration = (minutes * 60) + seconds;
 
     positionHandle(position) {
+        debugger;
+        let rangeslider;
+        let rangesliderHandle;
+        let progressbarWidth;
+        rangeslider = document.querySelector('.rangeslider').offsetWidth;
+        rangesliderHandle = document.querySelector('.rangeslider_handle').offsetWidth;
+        console.log("handling position");
         // Width of the progress bar
-        let progressbarWidth = this.rangeslider.current.offsetWidth - this.rangesliderHandle.current.offsetWidth;
+        progressbarWidth = rangeslider - rangesliderHandle;
 
         // Left position of the handle
-        let handleLeft = position - this.rangeslider.current.offsetLeft;
+        let handleLeft = position - rangeslider.offsetLeft;
 
         if (handleLeft >= 0 && handleLeft <= progressbarWidth) {
-            console.log("in between");
-            this.rangesliderHandle.current.style.marginLeft = handleLeft + "px";
+            // this.rangesliderHandle.current.style.left = handleLeft + "px";
+            this.setState({
+                playheadPos: `${handleLeft}px`
+            }); 
         }
         if (handleLeft < 0) {
-            console.log("left");
-            this.rangesliderHandle.current.style.marginLeft = "0px";
+            // this.rangesliderHandle.current.style.left = "0px";
+            this.setState({
+                playheadPos: "0px"
+            }); 
         }
         if (handleLeft > progressbarWidth) {
-            console.log("right");
-            this.rangesliderHandle.current.style.marginLeft = progressbarWidth + "px";
+            // this.rangesliderHandle.current.style.left = progressbarWidth + "px";
+            this.setState({
+                playheadPos: `${progressbarWidth}px`
+            }); 
         }
     }
 
     mouseMove(e) {
+        // debugger;
+        let rangeslider;
+        rangeslider = document.querySelector('.rangeslider');
         this.positionHandle(e.pageX);
-        this.audio.current.currentTime = (e.pageX / this.rangeslider.current.offsetWidth) * this.audio.current.duration;
+        let track;
+        track = document.querySelector('#audio');
+        track.currentTime = (e.pageX / rangeslider.offsetWidth) * track.duration;
     };
 
     mouseUp(e) {
@@ -210,7 +254,12 @@ class Player extends React.Component {
         window.addEventListener('mouseup', this.mouseUp);
     };
 
+    // findDuration() {
+    //     return document.querySelector('#audio').duration;
+    // }
+
     render() {
+
         return (
 
             <div className="player">
@@ -218,12 +267,12 @@ class Player extends React.Component {
 
                     {/* left side */}
                     <div className="now-playing-left">
-                        <div className="thumb-image"></div>
+                        <div className="thumb-image" style={{ backgroundImage: `url(${this.state.track.artwork})` }}></div>
                         <div className="np-container">
-                            <a id="npt" href="">Track</a>
+                            <a id="npt" href="">{ this.state.track.title }</a>
                             <div className="now-playing-text">
                         </div>
-                            <a id="npa" href="">Artist Name</a>
+                            <a id="npa" href="">{this.state.track.artist}</a>
                         </div>
                         <div className="love-container">
                             <div className="tooltip">Save to your Liked Songs</div>
@@ -235,50 +284,51 @@ class Player extends React.Component {
 
 
                     {/* Player Controls */}
-                    <div className="now-playing-controls">
-                        <div className="now-playing-buttons">
+                    <div className="now-playing-controls-container">
+                        <div className="now-playing-controls">
+                            <div className="now-playing-buttons">
 
-                            {/* shuffle button */}
-                            <button id="np-button">
-                                <img id="shuffle" src="shuffle_white.png" />
-                            </button>
+                                {/* shuffle button */}
+                                <button id="np-button">
+                                    <img id="shuffle" src="shuffle_white.png" />
+                                </button>
 
-                            {/* back button */}
-                            <button id="np-button">
-                                <img id="direction" src="previous_white.png" />
-                            </button>
+                                {/* back button */}
+                                <button id="np-button">
+                                    <img id="direction" src="previous_white.png" />
+                                </button>
 
-                            {/* play / pause buttons */}
-                            <button onClick={this.playAudio} id="np-button">
-                                <img id="play" src={ this.state.playPauseButton } />
-                                {/* <img id="play" src="play_white.png" /> */}
-                            </button>
+                                {/* play / pause buttons */}
+                                <button onClick={this.playAudio} id="np-button">
+                                    <img id="play" src={ this.state.playPauseButton } />
+                                    {/* <img id="play" src="play_white.png" /> */}
+                                </button>
 
-                            {/* next button */}
-                            <button id="np-button">
-                                <img id="direction" src="next_white.png" />
-                            </button>
+                                {/* next button */}
+                                <button id="np-button">
+                                    <img id="direction" src="next_white.png" />
+                                </button>
 
-                            {/* repeat button */}
-                            <button id="np-button">
-                                <img id="repeat" src="repeat_white.png" />
-                            </button>
+                                {/* repeat button */}
+                                <button id="np-button">
+                                    <img id="repeat" src="repeat_white.png" />
+                                </button>
+                            </div>
+                            {/* <div className="slidecontainer">
+                                <input type="range" min="0" max="100" defaultValue="0" className="slider" id="progressBar"></input>
+                            </div> */}
+                            <div className="rangeslider" onClick={ this.mouseMove } ref={ this.rangeslider } >
+                                <div className="rangeslider_fill" ref={ this.rangesliderFill }></div>
+                                <div className="rangeslider_handle" onMouseDown={ this.mouseDown } ref={ this.rangesliderHandle } style={ { left: this.state.playheadPos } }></div>
+                            </div>
+                            {/* <audio id="audio"><source src={ this.state.audioSrc } ref={ this.audio } onTimeUpdate={ this.timeUpdate }/></audio> */}
+                            <audio id="audio"><source src={ this.state.track.src } ref={ this.audio } /></audio>
+                            {/* { this.state.track } */}
+                        </div>  
+                        <div className="duration">
+                            {this.state.track.duration}
                         </div>
-                        {/* <div className="slidecontainer">
-                            <input type="range" min="0" max="100" defaultValue="0" className="slider" id="progressBar"></input>
-                        </div> */}
-                        <div className="rangeslider" onClick={ this.mouseMove } ref={ this.rangeslider } >
-                            <div className="rangeslider_fill" ref={ this.rangesliderFill }></div>
-                            <div className="rangeslider_handle" onMouseDown={ this.mouseDown } ref={ this.rangesliderHandle }></div>
-                        </div>
-                        <audio id="audio"><source src={this.state.audioSrc} ref={ this.audio }/></audio>
-                        {/* { this.state.track } */}
-                    </div>                   
-
-                    {/* <ReactAudioPlayer id="react-audio"
-                        src="skylines.mp3"
-                        controls
-                    /> */}
+                    </div>   
 
                     {/* TODO: Decide what's going on the right-hand side of the now-playing bar*/}
                     <div className="now-playing-right">
