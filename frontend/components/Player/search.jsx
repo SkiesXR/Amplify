3df630001a0e93a9f1e9b392976d3c76a9bc7d76
SkiesXR <pkrasnick@gmail.com>
2019-08-06 21:35:51 -0700

    Search: Add local state to manage search input value

--- frontend/components/Player/search.jsx
+++ frontend/components/Player/search.jsx
@@ -1,12 +1,34 @@
 import React from 'react';
 
 class Search extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            input: ''
+        };
+    }
+
+    componentDidMount() {
+        // this.props.receiveSearchResults();
+    }
+
+    update(field) {
+        return e => this.setState({
+            [field]: e.currentTarget.value
+        });
+    }
+
     render() {
         return(
             <div className="search-container">
                 <div className="search-inputBox">
                     <div className="search-contentSpacing">
-                        <input type="text" placeholder="Start typing..."/>
+                        <input 
+                            type="text"
+                            value={ this.state.input }
+                            placeholder="Start typing..."
+                            onChange={ this.update('input') }/>
                     </div>
                 </div>
                 <div className="search-content-empty">
