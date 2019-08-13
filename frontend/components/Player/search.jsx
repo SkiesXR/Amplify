cd8d082f7bdf169eeccc0ff0d4dc781b4d3d3095
SkiesXR <pkrasnick@gmail.com>
2019-08-13 12:43:39 -0700

    Search: Refactor podcast search results slice of state

--- frontend/components/Player/search.jsx
+++ frontend/components/Player/search.jsx
@@ -12,6 +12,7 @@ class Search extends React.Component {
         this.displayArtists = this.displayArtists.bind(this);
         this.displayAlbums = this.displayAlbums.bind(this);
         this.displayGenres = this.displayGenres.bind(this);
+        this.displayPodcasts = this.displayPodcasts.bind(this);
     }
 
     componentDidMount() {
@@ -83,7 +84,6 @@ class Search extends React.Component {
 
     displayGenres(genreResults) {
         let genres = genreResults[1];
-        debugger;
         return (
             <div>
                 <h1 className="main-h1">Genres</h1>
@@ -111,104 +111,42 @@ class Search extends React.Component {
         );
     }
 
-    // displayResult(result) {
-    //     switch (result[0]) {
-    //         case "albums":
-    //             debugger;
-    //             return result[1].map(album => {
-    //                 debugger;
-    //                 return (
-    //                     <div id="results-albums">
-    //                         <h1 className="main-h1">Albums</h1>
-    //                         <div className="album-artist-container">
-    //                             <div className="image-hover-container">
-    //                                 <Link to={`/albums/${album.id}`}><img src={album.album_art} />
-    //                                     <div className="Mike">
-    //                                         <button id="Mike-button">
-    //                                             <img id="Mike" src="play_white.png" />
-    //                                         </button>
-    //                                     </div>
-    //                                 </Link>
-    //                             </div>
-    //                             <div className="artist-container">
-    //                                 <Link id="grid-artist" to={`/albums/${album.id}`}>{album.title}</Link>
-    //                             </div>
-    //                         </div>
-    //                     </div>
-    //                 );
-    //             });
-    //             console.log(result[1]);
-    //             break;
-    //         case "artists":
-    //             return (
-    //                 <div id="results-artists">
-    //                     <h1 className="main-h1">Artists</h1>
-    //                     <div className="album-artist-container">
-    //                         <div className="artist-image-hover-container">
-    //                             <img src={result[1].artist_photo} />
-    //                         </div>
-    //                         <div className="title-container">
-    //                             <Link id="grid-title" to={`/artists/${result[1].id}`}>{result[1].name}</Link>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             );
-    //             break;
-    //         case "genres":
-    //             return (
-    //                 <div id="results-genres">
-    //                     <h1 className="main-h1">Genres</h1>
-    //                     <div className="album-artist-container">
-    //                         <div className="image-hover-container">
-    //                             <Link to={`/genres/${result[1].id}`}><img src={result[1].genre_image} />
-    //                                 <div className="Mike">
-    //                                     <button id="Mike-button">
-    //                                         <img id="Mike" src="play_white.png" />
-    //                                     </button>
-    //                                 </div>
-    //                             </Link>
-    //                         </div>
-    //                         <div className="artist-container">
-    //                             <Link id="grid-artist" to={`/genres/${result[1].id}`}>{result[1].category}</Link>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             );
-    //             break;
-    //         case "shows":
-    //             return (
-    //                 <div id="results-shows">
-    //                     <h1 className="main-h1">Podcasts</h1>
-    //                     <div className="album-artist-container">
-    //                         <div className="image-hover-container">
-    //                             <Link to=""><img src={result[1].show_photo} />
-    //                                 <div className="Mike">
-    //                                     <button id="Mike-button">
-    //                                         <img id="Mike" src="play_white.png" />
-    //                                     </button>
-    //                                 </div>
-    //                             </Link>
-    //                         </div>
-    //                         <div className="artist-container">
-    //                             <Link id="grid-artist" to={`/genres/${result[1].id}`}>{result[1].title}</Link>
-    //                         </div>
-    //                     </div>
-    //                 </div>
-    //             );
-    //             break;
-    //     }
-    // }
+    displayPodcasts(podcastResults) {
+        let podcasts = podcastResults[1];
+        return (
+            <div>
+                <h1 className="main-h1">Podcasts</h1>
+                <div className="results">
+                    {podcasts.map(podcast => {
+                        return (
+                            <div className="album-artist-container">
+                                <div className="image-hover-container">
+                                    <Link to={`/shows/${podcast.id}`}><img src={podcast.show_photo} />
+                                        <div className="Mike">
+                                            <button id="Mike-button">
+                                                <img id="Mike" src="play_white.png" />
+                                            </button>
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div className="artist-container">
+                                    <Link id="grid-artist" to={`/shows/${podcast.id}`}>{podcast.title}</Link>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        );
+    }
 
     render() {
-        // debugger;
         const { results = {} } = this.props;
         let searchResults = Object.entries(results).filter(([k,v]) => {return v != undefined}) || [];
         let artistResults = searchResults.filter(result => { return result[0] === "artists" })[0] || [];
         let albumResults = searchResults.filter(result => { return result[0] === "albums" })[0] || [];
         let genreResults = searchResults.filter(result => { return result[0] === "genres" })[0] || [];
         let podcastResults = searchResults.filter(result => { return result[0] === "shows" })[0] || [];
-        // debugger;
-        // let shownResults = searchResults.map(result => { return this.displayResult(result);});
             
         return(
             <div className="search-container">
@@ -221,7 +159,6 @@ class Search extends React.Component {
                             onChange={this.update('input')}/>
                     </div>
                 </div>
-                {/* <div className="search-content-empty"> */}
                 <div className={ Object.values(results).every(value => (value === undefined)) ? "search-content-empty" : "search-content-full" }>
                     <span id="search-amplify-header">Search Amplify</span>
                     <span id="search-amplify-subheader">Find your favorite songs, artists, albums, podcasts and playlists.</span>
@@ -230,6 +167,7 @@ class Search extends React.Component {
                     { artistResults.length > 0 ? this.displayArtists(artistResults) : ""}
                     { albumResults.length > 0 ? this.displayAlbums(albumResults) : ""}
                     { genreResults.length > 0 ? this.displayGenres(genreResults) : ""}
+                    { podcastResults.length > 0 ? this.displayPodcasts(podcastResults) : ""}
                 </div>
             </div>
         );
@@ -237,3 +175,5 @@ class Search extends React.Component {
 }
 
 export default Search;
+
+// TODO: Reset search results on component unmount
