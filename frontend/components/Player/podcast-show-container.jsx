c5b668e3e10597e4b82661aa6091914078d81173
SkiesXR <pkrasnick@gmail.com>
2019-08-15 14:50:00 -0700

    Add msp and mdp for podcast show container

--- frontend/components/Player/podcast-show-container.jsx
+++ frontend/components/Player/podcast-show-container.jsx
@@ -1,11 +1,26 @@
 import { connect } from "react-redux";
 import PodcastShow from "./podcast-show";
+import { fetchShow } from "../../actions/show_actions";
+import {
+  setCurrentSong,
+  toggleSong,
+  setQueue,
+  setPlaying
+} from "../../actions/player_actions";
 
 const msp = state => {
-  return {};
+  return {
+    podcast: state.entities.shows[ownProps.match.params.showId] || {}
+  };
 };
 
-const mdp = dispatch => ({});
+const mdp = dispatch => ({
+  fetchShow: id => dispatch(fetchShow(id)),
+  setCurrentSong: song => dispatch(setCurrentSong(song)),
+  toggleSong: () => dispatch(toggleSong()),
+  setPlaying: playing => dispatch(setPlaying(playing)),
+  setQueue: queue => dispatch(setQueue(queue))
+});
 
 export default connect(
   msp,
