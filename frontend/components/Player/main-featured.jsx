bda2f87ef52d0bfff961b5c1ee4230d09e3cbca9
SkiesXR <pkrasnick@gmail.com>
2019-08-15 10:45:55 -0700

    Fetch all models once user logs in

--- frontend/components/Player/main-featured.jsx
+++ frontend/components/Player/main-featured.jsx
@@ -1,29 +1,29 @@
-import React from 'react';
-import GridItem from './grid_item';
+import React from "react";
+import GridItem from "./grid_item";
 
 class MainFeatured extends React.Component {
+  componentDidMount() {
+    this.props.fetchArtists();
+    this.props.fetchAlbums();
+    this.props.fetchPlaylists();
+    this.props.fetchGenres();
+    this.props.fetchShows();
+  }
 
-    componentDidMount() {
-        this.props.fetchArtists();
-        this.props.fetchAlbums();
-    }
-
-    render() {
-        let albums = this.props.albums.map(album => {
-            return <GridItem key={ album.title }album={ album }/>
-        })
-        // debugger;
-        return (
-            <div>
-                <div className="header-grid">
-                    <h1 className="main-h1">Albums</h1>
-                </div>
-                <div className="featured-container">
-                    { albums }
-                </div>
-            </div>
-        );
-    }
+  render() {
+    let albums = this.props.albums.map(album => {
+      return <GridItem key={album.title} album={album} />;
+    });
+    // debugger;
+    return (
+      <div>
+        <div className="header-grid">
+          <h1 className="main-h1">Albums</h1>
+        </div>
+        <div className="featured-container">{albums}</div>
+      </div>
+    );
+  }
 }
 
 export default MainFeatured;
--- frontend/components/Player/main-featured_container.jsx
+++ frontend/components/Player/main-featured_container.jsx
@@ -1,20 +1,28 @@
-import { connect } from 'react-redux';
-import { fetchArtists } from '../../actions/artist.actions';
-import { fetchAlbums } from '../../actions/album_actions';
-import MainFeatured from '../Player/main-featured';
-
+import { connect } from "react-redux";
+import { fetchArtists } from "../../actions/artist.actions";
+import { fetchAlbums } from "../../actions/album_actions";
+import { fetchPlaylists } from "../../actions/playlist.actions";
+import { fetchGenres } from "../../actions/genre.actions";
+import { fetchShows } from "../../actions/show_actions";
+import MainFeatured from "../Player/main-featured";
 
 const msp = state => {
-    return {
-        currentUser: state.entities.users[1],
-        artists: Object.values(state.entities.artists),
-        albums: Object.values(state.entities.albums)
-    };
+  return {
+    currentUser: state.entities.users[1],
+    artists: Object.values(state.entities.artists),
+    albums: Object.values(state.entities.albums)
+  };
 };
 
 const mdp = dispatch => ({
-    fetchArtists: () => dispatch(fetchArtists()),
-    fetchAlbums: () => dispatch(fetchAlbums())
+  fetchArtists: () => dispatch(fetchArtists()),
+  fetchAlbums: () => dispatch(fetchAlbums()),
+  fetchPlaylists: () => dispatch(fetchPlaylists()),
+  fetchGenres: () => dispatch(fetchGenres()),
+  fetchShows: () => dispatch(fetchShows())
 });
 
-export default connect(msp, mdp)(MainFeatured);
+export default connect(
+  msp,
+  mdp
+)(MainFeatured);
