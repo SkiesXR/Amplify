071b21a9e9f266f1fcc336b77f4a730638c31ee7
SkiesXR <pkrasnick@gmail.com>
2019-08-25 23:41:19 -0700

    Add like model associations

--- app/models/like.rb
+++ app/models/like.rb
@@ -13,4 +13,14 @@ class Like < ApplicationRecord
 
     validates :user_id, :track_id, presence: true
 
+    belongs_to :user,
+        primary_key: :id,
+        foreign_key: :user_id,
+        class_name: :User
+
+    belongs_to :track,
+        primary_key: :id,
+        foreign_key: :track_id,
+        class_name: :Track
+
 end
