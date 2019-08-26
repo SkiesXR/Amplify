6ad03cee7468d43c206651743f5af4f7550d0d9f
SkiesXR <pkrasnick@gmail.com>
2019-08-26 00:28:32 -0700

    Add receiveLikedTracks action

--- frontend/actions/track_actions.js
+++ frontend/actions/track_actions.js
@@ -2,6 +2,7 @@ import * as TrackAPIUtil from '../util/track_api_util';
 
 export const RECEIVE_TRACKS = "RECEIVE_TRACKS";
 export const RECEIVE_TRACK = "RECEIVE_TRACK";
+export const RECEIVE_LIKED_TRACKS = "RECEIVE_LIKED_TRACKS";
 
 export const recieveTracks = (tracks) => ({
     type: RECEIVE_TRACKS,
@@ -11,6 +12,11 @@ export const recieveTracks = (tracks) => ({
 export const recieveTrack = (track) => ({
     type: RECEIVE_TRACK,
     track
+})
+
+export const receiveLikedTracks = (tracks) => ({
+    type: RECEIVE_LIKED_TRACKS,
+    tracks
 })
 
 export const fetchTracks = () => dispatch => (
