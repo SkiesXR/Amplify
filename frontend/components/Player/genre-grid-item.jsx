b47f083843bc0269558ea8bf2f52c30958ccd642
SkiesXR <pkrasnick@gmail.com>
2019-08-27 11:00:03 -0700

    Add placeholder images for genre index

--- frontend/components/Player/genre-grid-item.jsx
+++ frontend/components/Player/genre-grid-item.jsx
@@ -4,6 +4,16 @@ import { Link } from "react-router-dom";
 class GenreGridItem extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      loaded: false
+    };
+    this.setLoaded = this.setLoaded.bind(this);
+  }
+
+  setLoaded(boolean) {
+    this.setState({
+      loaded: true
+    });
   }
 
   render() {
@@ -12,8 +22,17 @@ class GenreGridItem extends React.Component {
     return (
       <div className="album-artist-container">
         <div className="genre-image-hover-container">
+          <div>
+            {!this.state.loaded ? (
+              <img src="AlbumArt-PlaceholderWithIcon.png" />
+            ) : null}
+          </div>
           <Link id="grid-title" to={`/genres/${this.props.genreId}`}>
-            <img src={genre_image} />
+            <img
+              src={genre_image}
+              style={!this.state.loaded ? { visibility: "hidden" } : {}}
+              onLoad={() => this.setLoaded(true)}
+            />
           </Link>
         </div>
 
